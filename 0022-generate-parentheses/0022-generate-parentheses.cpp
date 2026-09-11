75250209abbadfb2f8d19cf2bb426bef1c4d64be
Niraj Prasad <nirajprasad505@gmail.com>
2026-09-11 22:57:02 +0530

    Time: 4 ms (30.87%), Space: 16.1 MB (24.68%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> result;
+
+    void backtrack(string current, int open, int close, int n) {
+
+        // Valid combination completed
+        if (current.size() == 2 * n) {
+            result.push_back(current);
+            return;
+        }
+
+        // Add '('
+        if (open < n) {
+            backtrack(current + "(", open + 1, close, n);
+        }
+
+        // Add ')'
+        if (close < open) {
+            backtrack(current + ")", open, close + 1, n);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        backtrack("", 0, 0, n);
+        return result;
+        
+    }
+};
